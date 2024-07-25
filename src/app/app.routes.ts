d9de47d3d0bd82c0ec937629656134e8a68c295c
Evan Haggard <61989628+flyaway12345@users.noreply.github.com>
2024-07-25 03:10:31 -0400

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { KingPotusComponent } from './body/king-potus/king-potus.component';
-import { Error404Component } from './app/error404/error404.component';
-import { BodyComponent } from './body/body.component';
-
-export const routes: Routes = [
-    {path: '' ,component: BodyComponent},
-    {path: '**' ,component: Error404Component}
-];
